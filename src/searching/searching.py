0b7e408042516b3ecf47212c68ec8a687cc995be
johnkirtley <kirtleyj16@gmail.com>
2020-07-20 16:36:34 -0700

    :fire: Completed Binary Search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,6 +10,32 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    # set start and end pointers
+    floor = -1
+    ceiling = len(arr) - 1
 
-    return -1  # not found
+    while floor + 1 < ceiling:
+
+        # get total distance
+        distance = ceiling - floor
+
+        # find middle value
+        mid = round(distance / 2)
+        guess_index = floor + mid
+
+        # set middle index as initial search location
+        guess_value = arr[guess_index]
+
+        if guess_value == target:
+            return arr.index(guess_value)
+
+        # if target greater than mid, search right half of array
+        elif guess_value < target:
+            floor = guess_index
+
+        # if target less than mid, search left half of array
+        else:
+            ceiling = guess_index
+
+    # target not found
+    return -1
